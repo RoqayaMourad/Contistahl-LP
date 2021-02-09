d30bba5c65743d1b2df7b829b539a06413884ddd
roqaya299 <elephantsong29@example.com>
2021-02-09 23:51:02 +0200

    mobile version

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $headers = 'From: Contistahl Group Website <contistahl@lp-quadsolutions.com> ' . "\r\n" .
+    'Reply-To: contistahl@lp-quadsolutions.com ' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    $EmailTo = "emmasmith.quad@gmail.com";
+    $Body = "";
+    $Body .= "Name: ";
+    $Body .= $name;
+    $Body .= "\n";
+    $Body .= "Phone: ";
+    $Body .= $phone;
+    $Body .= "\n";
+    $Body .= "Email: ";
+    $Body .= $email;
+    $Body .= "\n";
+    $Body .= "Message: ";
+    $Body .= $message;
+    $Body .= "\n";
+    $Body .= "\n";
+    $Body .= "\n";
+    $Body .= "This Is Message Sent From Contistahl Group)";
+
+    $Subject = "Contact Form Submission(".$name.")";
+
+    // send email 
+    if(str_replace(' ', '', $name) != "" && str_replace(' ', '', $email)  != "" && str_replace(' ', '', $phone) != "" && str_replace(' ', '', $car_model) != ""){
+        $data['success'] = mail($EmailTo, $Subject, $Body, $headers);
+      
+    }else{
+        $data['success'] = "false";
+    }
+    // redirect to success page 
+    if ($data['success']){
+        //echo json_encode($data);
+        print "<meta http-equiv=\"refresh\" content=\"0;URL=thank-you.html\">";
+    }
+    else{
+        // $data["success"] = $success  ;
+        // echo json_encode($data);
+        print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
+    }
--- js/script.js
+++ js/script.js
@@ -21,9 +21,30 @@ window.onload = function() {
 	moveDivisor();
 };
 
+// $('.cta-btn').on("click",function(){
+//   $(window).scrollTop(0);
+// });
+
+
+if (window.innerWidth > 767.98) {       
+  $("#headerSection").css({"min-height":window.innerHeight - $("#topNav").height() + "px"});
+  $("#headerSection>div").css({"min-height":window.innerHeight - $("#topNav").height() + "px"});
+  $("#headerSection>div>div").css({"min-height":window.innerHeight - $("#topNav").height() + "px"});
+}else{
+  $("#headerSection").css({"min-height":"unset"});
+  $("#headerSection>div").css({"min-height":"unset"});
+  $("#headerSection>div>div").css({"min-height":"unset"});
+}
 
 $(window).on('resize once onload change', function(){
-  $("#headerSection").css({"min-height":window.innerHeight - $("#topNav").height() + "px"})
-  $("#headerSection>div").css({"min-height":window.innerHeight - $("#topNav").height() + "px"})
-  $("#headerSection>div>div").css({"min-height":window.innerHeight - $("#topNav").height() + "px"})
+  if (window.innerWidth > 767.98) {       
+      $("#headerSection").css({"min-height":window.innerHeight - $("#topNav").height() + "px"});
+      $("#headerSection>div").css({"min-height":window.innerHeight - $("#topNav").height() + "px"});
+      $("#headerSection>div>div").css({"min-height":window.innerHeight - $("#topNav").height() + "px"});
+  }else{
+      $("#headerSection").css({"min-height":"unset"});
+      $("#headerSection>div").css({"min-height":"unset"});
+      $("#headerSection>div>div").css({"min-height":"unset"});
+  }
+  
 });
